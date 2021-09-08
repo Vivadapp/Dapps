ac2a0b01e5b75376c4737be49a88cee376fdc88d
Vivadapp <68351400+Vivadapp@users.noreply.github.com>
2021-09-08 18:51:31 +0200

    String_manipulation

---
+++ day11-string-manipulation/tests/test/string-manipulation.js
@@ -0,0 +1,19 @@
+const Strings = artifacts.require('Strings');
+
+contract('Strings', () => {
+  let strings = null;
+  before(async () => {
+    strings = await Strings.deployed();
+  });
+
+  it('Should return the length of a string', async () => {
+    const length = await strings.length('abc');
+    assert(length.toNumber() === 3);
+  });
+
+  it('Should concatenate 2 strings', async() => {
+    const concatenatedString = await strings.concatenate('abc', 'def');
+    assert(concatenatedString === 'abcdef');
+  });
+});
+
