ff9d32b46da7d64607c66a8fd2c041ad11ed24ab
Vivadapp <68351400+Vivadapp@users.noreply.github.com>
2021-09-27 11:25:49 +0200

    Modif

--- DEED_multi_payouts/frontend/migrations/2_deed.js
+++
@@ -1,5 +0,0 @@
-const DeedMultiPayouts = artifacts.require("DeedMultiPayout");
-
-module.exports = function(deployer, _network, accounts) {
-  deployer.deploy(DeedMultiPayouts, accounts[0], accounts[1], 60);
-};
